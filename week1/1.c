cdbda2826f7dec5946b9a5b49154a14ffcba98e4
Ashmeet Chhabra <94653350+ashmeet-chhabra@users.noreply.github.com>
2023-02-15 09:27:27 +0530

    Update 1.c

--- week1/1.c
+++ week1/1.c
@@ -1,46 +1,47 @@
 #include <stdio.h>
 
-int main() {
-    int n, key;
-    int found = 0; // flag
-    int comp = 0; // comparisons
+int main()
+{
+	int t;
+	
+	printf("Enter the number of test cases");
+	scanf("%d", &t); 
+	
+	while(t--)
+	{
+    	int n, key;
+	    int found = 0; // flag
+    	int comp = 0; // comparisons
 
-    printf("Enter size of array: ");
-    scanf("%d", &n);
+    	printf("Enter size of array: ");
+    	scanf("%d", &n);
 
-    int arr[n];
+    	int arr[n];
 
-    printf("Enter %d elements: ", n);
+    	printf("Enter %d elements: ", n);
 
-    for(int i = 0; i < n; ++i)
-        scanf("%d", &arr[i]);
+    	for(int i = 0; i < n; ++i)
+    	    scanf("%d", &arr[i]);
 
-    printf("Enter key: ");
-    scanf("%d", &key);
+    	printf("Enter key: ");
+    	scanf("%d", &key);
 
-    for(int i = 0; i < n; ++i)
-        if(++comp && arr[i] == key)
-        {
-            found = 1;
-            break;
-        }
+    	for(int i = 0; i < n; ++i)
+    	    if(++comp && arr[i] == key)
+    	    {
+    	        found = 1;
+    	        break;
+    	    }
 
-    if(found)
-        printf("Present %d", comp);
-    else
-        printf("Not Present %d", comp);
-
+    	if(found)
+    	    printf("Present %d", comp);
+    	else
+    	    printf("Not Present %d", comp);
+    }
     return 0;
 }
 
 /*
-Enter size of array: 5
-Enter 5 elements: 1 4 1 2 9
-Enter key: 9
-Present 5
 
-Enter size of array: 5
-Enter 5 elements: 3 7 1 0 6
-Enter key: 1
-Present 3
+
 */
